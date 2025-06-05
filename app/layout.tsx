f4dd53de288a115696263e04b1a1edccdc6c01c1
Diego Oruezabal <diegooruezabal@MacBook-Pro-de-Diego.local>
2025-06-05 15:49:19 +0200

    Navbar y sidebar trigger

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/ui/sidebar"
 import AppSidebar from "./(routes)/(root)/components/AppSidebar/AppSidebar";
+import { Navbar } from "@/components/Shared";
 
 const spaceGrotesk = Space_Grotesk ({
   variable: "--font-space-grotesk",
@@ -27,8 +28,12 @@ export default function RootLayout({
         className={`${spaceGrotesk.variable} antialiased`}
       >
             <SidebarProvider>
-              <AppSidebar />    
-                 <main className="flex-1"> {children}</main> 
+              <AppSidebar />  
+              <div className="w-full bg-stone-100 flex flex-col min-h-screen">
+                    <Navbar />
+            
+                     <main className="flex-1"> {children}</main> 
+              </div>  
             </SidebarProvider>     
       </body>
     </html>
